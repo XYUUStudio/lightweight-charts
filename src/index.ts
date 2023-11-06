2d13bf163e1881a18581e919a8358a432086f356
XYUU <xyuu@xyuu.net>
2023-11-06 22:46:16 +0800

    更新为库函数，导出画图所需的类

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,23 @@ export {
 
 export { createChart } from './api/create-chart';
 
+export { ChartWidget } from './gui/chart-widget';
+export { PaneWidget } from './gui/pane-widget';
+export { CursorType, PriceAxisWidget } from './gui/price-axis-widget';
+export { assert, ensureNotNull } from './helpers/assertions';
+export { clone } from './helpers/strict-type-checks';
+export { AutoscaleInfoImpl } from './model/autoscale-info-impl';
+export { ChartModel } from './model/chart-model';
+export { CustomPriceLine } from './model/custom-price-line';
+export { Pane } from './model/pane';
+export { PriceDataSource } from './model/price-data-source';
+export { PriceScale } from './model/price-scale';
+export { TimeScale } from './model/time-scale';
+export { BitmapCoordinatesPaneRenderer } from './renderers/bitmap-coordinates-pane-renderer';
+export { CompositeRenderer } from './renderers/composite-renderer';
+export { CrosshairRenderer } from './renderers/crosshair-renderer';
+export { PriceAxisRendererOptionsProvider } from './renderers/price-axis-renderer-options-provider';
+export { drawCircle, hitTestCircle } from './renderers/series-markers-circle';
 /**
  * Returns the current version as a string. For example `'3.3.0'`.
  */
